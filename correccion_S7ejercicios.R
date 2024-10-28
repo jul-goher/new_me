4fc107097ffaa3133bb4373ec6d975812b34c64d
Juliet <juliegonzalezhrra@gmail.com>
2024-10-28 15:11:35 -0600

    Add files via upload

---
+++ correccion_S7ejercicios.R
@@ -0,0 +1,42 @@
+chain <- subset(proteins_167_161521, select = c (2, 5))
+View (chain) #cambiar chain a el original 
+
+proteins_167_161521 [which (proteins_167_161521$Strand == "+"), c("Strand", "Locus")] -> positive
+View (positive) 
+
+proteins_167_161521 [which (proteins_167_161521$Strand == "-"), c("Strand", "Locus")] -> negative
+View (negative)
+
+write.csv (positive, file = "Resultados/positive_ecoliK12.pdf")
+write.csv (negative, file = "Resultados/negative_ecoliK12.pdf")
+
+
+#Aquí no va el original, sino el de la anotacion 
+pdf ("Resultados/boxplotecoli.pdf")
+boxplot (proteins_167_161521 $ Length)
+dev.off ()
+#ahora sí sale 
+
+pdf("Resultados/boxplotecoli.pdf")
+boxplot (proteins_167_161521 $ Stop)
+dev.off ()
+
+#ahora sí sale 
+
+pdf("Resultados/boxplot2")
+boxplot (proteins_167_161521$Start - proteins_167_161521$Stop)
+dev.off
+
+min (ecolianotacion$lenght)
+mayor <- ecoliamptacion [ecolianotacion$lenght == max (ecolianotacion$length), "Protein.Name"]
+pequeño <- ecoliamptacion [ecolianotacion$lenght == min (ecolianotacion$length), "Protein.Name"]
+
+paste ("La proteina mayor es: ", mayor, "con un tamaño de ",  max(ecolianotacion$length), "aminoácidos")
+
+#10 más grandes, 10 más pequeños
+sort (ecoli_anotaciomn$lenght)[1:10]
+sort (ecoli_anotacion$Lenght, decreasing =TRUE) [1:10]
+
+dim (positive)[1] #lo puedo poner en una variable 
+dim (negative) [1]
+
